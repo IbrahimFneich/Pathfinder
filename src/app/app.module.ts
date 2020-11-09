84a74de91ba025736c30fbbe8536f652adae0e5d
Administrator <ibrahim1998fneich@hotmail.com>
2020-11-10 00:41:02 +0200

    zabbit mn ba3ed l khabsa 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NodeComponent } from './main/node/node.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    NodeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/wallsRandom.ts
@@ -0,0 +1,77 @@
+import { INode } from './INode';
+import { resetMatrix } from './resetMatrix';
+
+
+export function wallsRandom(grid, startNode, finishNode){
+    
+
+    const walls:INode[]=[];
+    //top row
+    for (let index = 0; index < 70; index++) {
+        let node:INode = grid[0][index];
+        node.isWall=true;
+        walls.push(node);
+    }
+
+    //right col
+    for (let index = 0; index < 25; index++) {
+        let node:INode = grid[index][69];
+        node.isWall=true;
+        walls.push(node);
+    }
+
+    //bottom row
+    for (let index = 69; index >= 0; index--) {
+        let node:INode = grid[24][index];
+        node.isWall=true;
+        walls.push(node);
+    }
+
+    //left col
+    for (let index = 24; index >= 0; index--) {
+        let node:INode = grid[index][0];
+        node.isWall=true;
+        walls.push(node);
+    }
+
+    //cols
+    for (let index = 2; index < 35; index+=2) {
+
+            let window = Math.floor(Math.random() * 23) + 1;
+            for (let j = 1; j < 24; j++) {
+                let node:INode = grid[j][index];    
+
+                if(j!=window){
+                    node.isWall=true;
+                }else{
+                    node.isWall=false;
+                }
+                 walls.push(node);
+                
+            }
+        
+    }
+    
+    //cols
+    for (let index = 68; index >= 35; index-=2) {
+            let window = Math.floor(Math.random() * 23) + 1;
+            for (let j = 1; j < 24; j++) {
+                let node:INode = grid[j][index];    
+
+                if(j!=window){
+                    node.isWall=true;
+                }else{
+                    node.isWall=false;
+                }
+                 walls.push(node);
+                
+            }
+        
+    }
+
+
+
+
+
+    return walls;
+}
